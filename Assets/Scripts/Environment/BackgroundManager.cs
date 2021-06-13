d33e57155c56a90e7632dea8c3fa1ba9c6299d9a
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2021-06-13 15:01:47 +0200

    Moving background and some models reimport

---
+++ Assets/Scripts/Environment/BackgroundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public class BackgroundManager : MonoBehaviour
+    {
+        private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+        
+        [SerializeField] private float scrollSpeed = 0.5f;
+        private Renderer _renderer;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer> ();
+        }
+
+        private void Update()
+        {
+            var offset = Time.time * scrollSpeed;
+            _renderer.material.SetTextureOffset(MainTex, new Vector2(0, offset));
+        }
+    }
+}
