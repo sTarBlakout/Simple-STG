70f223da9d4eaff10f4292ebe83452776c406ecf
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2021-06-15 20:03:11 +0200

    Created some start logic for GameController

---
+++ Assets/Scripts/Environment/SpawnArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArea : MonoBehaviour
+{
+    [SerializeField] private float width;
+
+    public Vector3 GetSpawnPosition()
+    {
+        return new Vector3(Random.Range(-width, width), transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Global/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float timeBetwWaves;
+    [SerializeField] private Vector2 timeBetwSpawns;
+    [SerializeField] private float waveHpMod;
+    [SerializeField] private float waveNumberMod;
+    [SerializeField] private List<GameObject> spaceObjectsForWaves = new List<GameObject>();
+    
+    [Header("Components")]
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Transform playerSpawnPosition;
+    [SerializeField] private SpawnArea spawnArea;
+    
+    private bool _isGameOver;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        _isGameOver = false;
+
+        Instantiate(playerPrefab, playerSpawnPosition.position, Quaternion.identity);
+        StartCoroutine(WaveSpawner());
+    }
+
+    private IEnumerator WaveSpawner()
+    {
+        var waveCounter = 1;
+
+        while (!_isGameOver)
+        {
+            yield return new WaitForSeconds(timeBetwWaves);
+            var spaceObjects = GetSpaceObjectsThisWave(waveCounter);
+            foreach (var spaceObject in spaceObjects)
+            {
+                Instantiate(spaceObject, spawnArea.GetSpawnPosition(), Quaternion.identity);
+                yield return new WaitForSeconds(Random.Range(timeBetwSpawns.x, timeBetwSpawns.y));
+            }
+
+            waveCounter++;
+        }
+    }
+
+    private List<GameObject> GetSpaceObjectsThisWave(int waveNumber)
+    {
+        var spaceObjects = new List<GameObject>();
+        var numberOfEnemies = Mathf.CeilToInt(waveNumber * waveNumberMod);
+
+        while (spaceObjects.Count != numberOfEnemies)
+        {
+            var spaceObject = spaceObjectsForWaves[Random.Range(0, spaceObjectsForWaves.Count)];
+            spaceObjects.Add(spaceObject);
+        }
+
+        return spaceObjects;
+    }
+}
